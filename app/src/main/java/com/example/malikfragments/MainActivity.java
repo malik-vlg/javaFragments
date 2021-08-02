f4c03d6e68e13203c458e39d47ef89ee4610889c
Malik <abdinov-malik@mail.ru>
2021-08-02 20:06:59 +0300

    Homework8

--- app/src/main/java/com/example/malikfragments/MainActivity.java
+++ app/src/main/java/com/example/malikfragments/MainActivity.java
@@ -1,15 +1,73 @@
 package com.example.malikfragments;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.SearchView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    FloatingActionButton fab;
+    Adapter adapter;
+    List<Model> notesList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycle_view);
+        fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddNotesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        notesList = new ArrayList<>();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new Adapter(this, MainActivity.this, notesList);
+        recyclerView.setAdapter(adapter);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.searchbar);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setQueryHint("Search Notes Here");
+
+        SearchView.OnQueryTextListener listener = new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return true;
+            }
+        };
+
+        searchView.setOnQueryTextListener(listener);
+        return super.onCreateOptionsMenu(menu);
     }
 
 }
